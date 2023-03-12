b9efc4749bea9e628f196578424595f516ba9491
Pere Bohigas <pere.bohigas@gmail.com>
2023-03-12 22:20:07 +0100

    [0065] Fix indentation

--- 0065-swiftui-and-state-management-pt1/StateManagement.playground/Contents.swift
+++ 0065-swiftui-and-state-management-pt1/StateManagement.playground/Contents.swift
@@ -1,28 +1,27 @@
-
 import SwiftUI
 
 struct ContentView: View {
-  @ObservedObject var state: AppState
+    @ObservedObject var state: AppState
 
-  var body: some View {
-    NavigationStack {
-      List {
-        NavigationLink(destination: CounterView(state: self.state)) {
-          Text("Counter demo")
+    var body: some View {
+        NavigationStack {
+            List {
+                NavigationLink(destination: CounterView(state: self.state)) {
+                    Text("Counter demo")
+                }
+                NavigationLink(destination: EmptyView()) {
+                    Text("Favorite primes")
+                }
+            }
+            .navigationBarTitle("State management")
         }
-        NavigationLink(destination: EmptyView()) {
-          Text("Favorite primes")
-        }
-      }
-      .navigationBarTitle("State management")
     }
-  }
 }
 
 private func ordinal(_ n: Int) -> String {
-  let formatter = NumberFormatter()
-  formatter.numberStyle = .ordinal
-  return formatter.string(for: n) ?? ""
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .ordinal
+    return formatter.string(for: n) ?? ""
 }
 
 //BindableObject
@@ -30,39 +29,39 @@ private func ordinal(_ n: Int) -> String {
 import Combine
 
 class AppState: ObservableObject {
-  @Published var count = 0
+    @Published var count = 0
 }
 
 struct CounterView: View {
-  @ObservedObject var state: AppState
+    @ObservedObject var state: AppState
 
-  var body: some View {
-    VStack {
-      HStack {
-        Button(action: { self.state.count -= 1 }) {
-          Text("-")
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: { self.state.count -= 1 }) {
+                    Text("-")
+                }
+                Text("\(self.state.count)")
+                Button(action: { self.state.count += 1 }) {
+                    Text("+")
+                }
+            }
+            Button(action: {}) {
+                Text("Is this prime?")
+            }
+            Button(action: {}) {
+                Text("What is the \(ordinal(self.state.count)) prime?")
+            }
         }
-        Text("\(self.state.count)")
-        Button(action: { self.state.count += 1 }) {
-          Text("+")
-        }
-      }
-      Button(action: {}) {
-        Text("Is this prime?")
-      }
-      Button(action: {}) {
-        Text("What is the \(ordinal(self.state.count)) prime?")
-      }
+        .font(.title)
+        .navigationBarTitle("Counter demo")
     }
-    .font(.title)
-    .navigationBarTitle("Counter demo")
-  }
 }
 
 
 import PlaygroundSupport
 
 PlaygroundPage.current.liveView = UIHostingController(
-  rootView: ContentView(state: AppState())
-//  rootView: CounterView()
+    rootView: ContentView(state: AppState())
+    //  rootView: CounterView()
 )
