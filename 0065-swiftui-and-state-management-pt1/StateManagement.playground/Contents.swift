c55934884f7e9928a8ebad769e2483fd0c5c6ab9
Pere Bohigas <pere.bohigas@gmail.com>
2023-03-12 23:06:42 +0100

    [0065] Add persistence of AppState across multiple runs using UserDefaults

--- 0065-swiftui-and-state-management-pt1/StateManagement.playground/Contents.swift
+++ 0065-swiftui-and-state-management-pt1/StateManagement.playground/Contents.swift
@@ -28,8 +28,43 @@ private func ordinal(_ n: Int) -> String {
 
 import Combine
 
-class AppState: ObservableObject {
-    @Published var count = 0
+class AppState: ObservableObject, Codable {
+    static private let userDefaultsKey: String = "AppState"
+
+    @Published var count: Int = 0 {
+        didSet {
+            save()
+        }
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case count
+    }
+
+    init() {
+        if
+            let data: Data = UserDefaults.standard.data(forKey: Self.userDefaultsKey),
+            let state: Self = try? JSONDecoder().decode(Self.self, from: data)
+        {
+            self.count = state.count
+        }
+    }
+
+    required init(from decoder:Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        count = try values.decode(Int.self, forKey: .count)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(count, forKey: .count)
+    }
+
+    private func save() {
+        if let encoded: Data = try? JSONEncoder().encode(self) {
+            UserDefaults.standard.set(encoded, forKey: Self.userDefaultsKey)
+        }
+    }
 }
 
 struct CounterView: View {
